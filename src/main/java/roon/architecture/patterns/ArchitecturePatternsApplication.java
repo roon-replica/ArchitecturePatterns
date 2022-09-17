65a4d526db97f999ae7d90b29641ff585dd4d767
leemr <think-roon@naver.com>
2022-09-18 00:03:21 +0900

    Initial commit

---
+++ src/main/java/roon/architecture/patterns/ArchitecturePatternsApplication.java
@@ -0,0 +1,13 @@
+package roon.architecture.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitecturePatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitecturePatternsApplication.class, args);
+    }
+
+}

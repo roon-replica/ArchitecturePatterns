287c1e01d712950c627bf90c185a9332152aae24
leemr <think-roon@naver.com>
2022-09-20 23:22:41 +0900

    데이터 접근에 DIP 적용하기

--- src/main/java/roon/architecture/patterns/ch2_repository/application/order/OrderService.java
+++ src/main/java/roon/architecture/patterns/ch2_repository/application/order/OrderService.java
@@ -2,6 +2,7 @@ package roon.architecture.patterns.ch2_repository.application.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import roon.architecture.patterns.ch2_repository.domain.order.Order;
 import roon.architecture.patterns.ch2_repository.domain.order.repository.OrderRepository;
 
@@ -10,6 +11,7 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Transactional
     public String createOrder(Order order){
         return orderRepository.save(order);
     }
